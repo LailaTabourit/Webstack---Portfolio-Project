8c2870036cf7b468bdbaf2a0014016adcfe4d1f3
laila tabourit <lailatabourit@gmail.com>
2024-03-20 12:17:05 +0000

    coffee shop

--- js/script.js
+++ js/script.js
@@ -39,3 +39,9 @@ var swiper = new Swiper(".review-slider", {
         }
     },
 });
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your Order passed successfully. Thank you for your trust');
+    this.reset();
+});
